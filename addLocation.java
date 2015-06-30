d93d2ddebe5059b49b3db54022e5f8530c6358a2
ghiyatshanif <ghiyatshanif@live.com>
2015-06-30 16:58:04 +0700

    add screenshot

---
+++ addLocation.java
@@ -0,0 +1,59 @@
+[33mcommit 4b9d8ee9e12bbb1b4da34b9e66ffbd9f2ad31e2b[m
+Author: ghiyatshanif <ghiyatshanif@live.com>
+Date:   Sun Jun 21 17:01:41 2015 +0700
+
+    fixed some bugs
+
+[33mcommit c6bd740b80e8ec1ebe391e69850ba6326cf75d8c[m
+Author: ghiyatshanif <ghiyatshanif@live.com>
+Date:   Sat Jun 20 18:16:47 2015 +0700
+
+    added icon launcher
+
+[33mcommit 1a491a5650f7b02470a3e1253a26cc73e905079e[m
+Author: ghiyatshanif <ghiyatshanif@live.com>
+Date:   Sat Jun 20 17:08:07 2015 +0700
+
+    added Konservasi, fixed backstack
+
+[33mcommit 4e951840fb5ad4d9a5058a86b09921a44db3958c[m
+Author: ghiyatshanif <ghiyatshanif@live.com>
+Date:   Fri Jun 19 07:34:04 2015 +0700
+
+    Added Chart Activity
+
+[33mcommit f3f9f49b01dfe971d362a4f06ae13c2cd0522c70[m
+Author: ghiyatshanif <ghiyatshanif@live.com>
+Date:   Fri Jun 19 07:25:57 2015 +0700
+
+    Rename file rangerAdapter.java
+
+[33mcommit 3d19f981c0bc90def6449ceff5b955b469b8052a[m
+Author: ghiyatshanif <ghiyatshanif@live.com>
+Date:   Fri Jun 19 07:23:27 2015 +0700
+
+    Added Chart Activity
+
+[33mcommit eb9c3b8ff608fd65ed85ecf795e305012312a7c3[m
+Author: ghiyatshanif <ghiyatshanif@live.com>
+Date:   Wed Jun 17 12:23:59 2015 +0700
+
+    changed update intgerval
+
+[33mcommit 695b94a3250e46dd0a7acad5c474d8e0d421c758[m
+Author: ghiyatshanif <ghiyatshanif@live.com>
+Date:   Wed Jun 17 11:59:51 2015 +0700
+
+    changed update intgerval
+
+[33mcommit e05e65254d9f7f6ce04283f53babeaab9cd8bced[m
+Author: ghiyatshanif <ghiyatshanif@live.com>
+Date:   Wed Jun 17 11:38:42 2015 +0700
+
+    initial commit
+
+[33mcommit 6d68ab65ca120f9c17e85ec59f6311c4ce0439e1[m
+Author: ghiyatshanif <ghiyatshanif@live.com>
+Date:   Wed Jun 17 11:35:21 2015 +0700
+
+    initial commit
